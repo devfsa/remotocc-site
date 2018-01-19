6a8e431aa077696800a658191fec5c0d6d6d2beb
Jonata Weber <jonataa@gmail.com>
2018-01-18 22:47:08 -0300

    add filter by tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,29 @@
 (function(window, document, location) {
 
-    var anchor = location.hash;
+    var tag = getTagNameFromUrl(location);
+    var jobItems = document.querySelectorAll('.job-item');
 
     window.addEventListener('hashchange', changeUrlHandler);
 
+    if (tag !== '') {
+        filterJobItemsByTag(tag);
+    }
+
     function changeUrlHandler() {
-        anchor = location.hash;
-        console.log(anchor);
+        tag = getTagNameFromUrl(location);
+        
+        filterJobItemsByTag(tag);
+    }
+
+    function filterJobItemsByTag(tag) {
+        jobItems.forEach(function(jobItem) {
+            var tags = jobItem.dataset.tags.split(',');
+            jobItem.style.display = tags.indexOf(tag) > -1 ? 'block' : 'none';
+        });
+    }
+
+    function getTagNameFromUrl(location) {
+        return location.hash.substr(1);
     }
 
 })(window, document, location);
