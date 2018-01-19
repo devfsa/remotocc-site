17d1a3d10b8a4d314dc1d87b3e46ac8a7b423bf6
Jonata Weber <jonataa@gmail.com>
2018-01-19 01:04:55 -0300

    add comments to javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,53 @@
 
     // job item html elements
     var jobItems = document.querySelectorAll('.job-item');
+    var filters = document.getElementById('filters');
+    var tagsFilterLabel = document.getElementById('tagsFilterLabel');
+    var clearFiltersBtn = document.getElementById('clearFiltersBtn');
 
+    // listening clear filters button
+    clearFiltersBtn.addEventListener('click', clearFiltersHandler);
+    console.log(filters);
+
+    // listening url change detection
     window.addEventListener('hashchange', changeUrlHandler);
 
     // filter by tag on load
     if (tag !== '') {
-        filterJobItemsByTag(tag);
+        filterJobItemsByTag(jobItems, tag, tagsFilterLabel);
     }
 
     // change url handler
     function changeUrlHandler() {
         tag = getTagNameFromUrl(location);
-        filterJobItemsByTag(tag);
+        filterJobItemsByTag(jobItems, tag, tagsFilterLabel);
     }
 
     // filter job item elements by tag name
-    function filterJobItemsByTag(tag) {
+    function filterJobItemsByTag(jobItems, tag, tagsFilterLabel) {
+        var tagIsEmpty = tag === '';
+        
+        tagsFilterLabel.innerHTML = '';
+        filters.style.visibility  = 'hidden';
+        
+        if (tagIsEmpty === false) {
+            tagsFilterLabel.innerHTML = tag;
+            filters.style.visibility = 'visible';
+        }
+
         jobItems.forEach(function(jobItem) {
             var tags = jobItem.dataset.tags.split(',');
-            jobItem.style.display = tags.indexOf(tag) > -1 ? 'block' : 'none';
+            var isShowJob = tags.indexOf(tag) > -1 || tagIsEmpty;
+            jobItem.style.display = isShowJob ? 'block' : 'none';
         });
+    }
+
+    // clear filter handler
+    function clearFiltersHandler() {
+        console.log('ok');
+        var emptyTag = '';
+        location.hash = '';
+        filterJobItemsByTag(jobItems, emptyTag, tagsFilterLabel);
     }
 
     // remote hastag symbol '#' from anchor url
